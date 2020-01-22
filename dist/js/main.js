97ed60d3b70677aadf9e32a5c61cf7d53165d5c4
TudorPetrariu <tudor.petrariu@yahoo.com>
2020-01-22 19:53:51 +0100

    changed menu icons and added animated css

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,6 +5,8 @@ const menu = document.querySelector('.menu')
 const menuNav = document.querySelector('.menu-nav')
 const menuBranding = document.querySelector('.menu-branding')
 const navItems = document.querySelectorAll('.nav-item')
+const portrait = document.querySelectorAll('.animated')
+console.log(portrait)
 
 console.log('hello')
 // set initial state of menu
@@ -13,19 +15,24 @@ menuBtn.addEventListener('click', toggleMenu)
 
 function toggleMenu() {
     if (!showMenu) {
+
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'))
+        portrait.forEach(pic => pic.classList.add('flipInX'))
 
 
         showMenu = true;
     } else {
+        portrait.forEach(pic => pic.classList.remove('flipInX'))
+
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
+
         navItems.forEach(item => item.classList.remove('show'))
 
 
