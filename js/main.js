0633759c4e3e83ce58619f98e8c57892ef5e3a09
TudorPetrariu <tudor.petrariu@yahoo.com>
2020-01-23 13:50:21 +0100

    Updates

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// select dom items
+
+const menuBtn = document.querySelector('.menu-btn')
+const menu = document.querySelector('.menu')
+const menuNav = document.querySelector('.menu-nav')
+const menuBranding = document.querySelector('.menu-branding')
+const navItems = document.querySelectorAll('.nav-item')
+const animation = document.querySelectorAll('.animated')
+let showMenu = false;
+menuBtn.addEventListener('click', toggleMenu)
+
+function toggleMenu() {
+    if (!showMenu) {
+        menuBtn.classList.add('close');
+        menu.classList.add('show');
+        menuNav.classList.add('show');
+        menuBranding.classList.add('show');
+        navItems.forEach(item => item.classList.add('show'))
+        animation.forEach(pic => pic.classList.add('flipInX'))
+        showMenu = true;
+    } else {
+        animation.forEach(pic => pic.classList.remove('flipInX'))
+        menuBtn.classList.remove('close');
+        menu.classList.remove('show');
+        menuNav.classList.remove('show');
+        menuBranding.classList.remove('show');
+        navItems.forEach(item => item.classList.remove('show'))
+        showMenu = false;
+    }
+}
