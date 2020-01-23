db513e4127a1f334f39ad4ead37be5ce167b4f2d
TudorPetrariu <tudor.petrariu@yahoo.com>
2020-01-23 12:30:03 +0100

    finished styling

--- dist/js/main.js
+++ dist/js/main.js
@@ -5,40 +5,26 @@ const menu = document.querySelector('.menu')
 const menuNav = document.querySelector('.menu-nav')
 const menuBranding = document.querySelector('.menu-branding')
 const navItems = document.querySelectorAll('.nav-item')
-const portrait = document.querySelectorAll('.animated')
-console.log(portrait)
-
-console.log('hello')
-// set initial state of menu
+const animation = document.querySelectorAll('.animated')
 let showMenu = false;
 menuBtn.addEventListener('click', toggleMenu)
 
 function toggleMenu() {
     if (!showMenu) {
-
         menuBtn.classList.add('close');
         menu.classList.add('show');
         menuNav.classList.add('show');
         menuBranding.classList.add('show');
         navItems.forEach(item => item.classList.add('show'))
-        portrait.forEach(pic => pic.classList.add('flipInX'))
-
-
+        animation.forEach(pic => pic.classList.add('flipInX'))
         showMenu = true;
     } else {
-        portrait.forEach(pic => pic.classList.remove('flipInX'))
-
+        animation.forEach(pic => pic.classList.remove('flipInX'))
         menuBtn.classList.remove('close');
         menu.classList.remove('show');
         menuNav.classList.remove('show');
         menuBranding.classList.remove('show');
-
         navItems.forEach(item => item.classList.remove('show'))
-
-
         showMenu = false;
-
-
-
     }
 }
